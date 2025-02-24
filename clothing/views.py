e7056490bc578d439669e5617759b7b37d18dfe2
saurabhkokane <kokanesaurabh91@gmail.com>
2025-02-24 14:50:29 +0530

    update

--- clothing/views.py
+++ clothing/views.py
@@ -1,17 +1,41 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from clothing.models import Product
 from .forms import ProductForm
 
-USD_TO_INR = 83  # Conversion rate
+# Create your views here.
 
-def fashion_view(request):
-    products = Product.objects.all()
+def clothing(request):
     form = ProductForm()
-
+    pro_img = Product.objects.all()
+    
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            form = ProductForm()  # Reset the form after saving
+            form = ProductForm() # Reset form after saving
+            
+    return render(request, 'clothing/fashion.html',{'pro_img': pro_img, 'form':form})
 
-    return render(request, 'clothing/fashion.html', {'products': products, 'form': form})
+
+def edit_product(request, product_id):
+    prod = get_object_or_404(Product, id=product_id)
+    
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=prod)
+        if form.is_valid():
+            form.save()
+            return redirect('clothing')
+    else:
+        form = ProductForm(instance=prod)
+        
+    return render(request, 'clothing/edit_product.html', {'form': form})
+
+
+def delete_product(request, product_id):
+    prod = get_object_or_404(Product, id=product_id)
+    
+    if request.method == "POST":
+        prod.delete()
+        return redirect('clothing')
+        
+    return render(request, 'clothing/confirm_delete.html', {'product': prod})
--- electronics/urls.py
+++ electronics/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from electronics import views
 
 urlpatterns=[
-    path('',views.home,name='home'),
+    path('',views.home,name="home"),
+    
 ]
